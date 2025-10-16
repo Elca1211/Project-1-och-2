31b3dda2c6e693c7dda9a3af3a5129ed6c235c6a
Elca1211 <ella.cavenius.3528@student.uu.se>
2025-10-16 10:54:15 +0200

    Add script for calculating dam force and visualization
    This script calculates the resultant force using the trapezoidal rule and visualizes the function f(y) related to dam force.

---
+++ Utan inerp.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+p = 1000 * 9.81 # Tryckkonstanten 
+y = np.array([0, 2, 4, 6, 8, 10, 12])
+w = np.array([10.00, 11.18, 12.18, 12.20, 13.00, 13.50, 14.00])
+H = 12.0 
+
+f = (H - y) * w
+
+h = 2
+
+trapets = h * ( 0.5*f[0] + f[1:-1].sum() +  0.5 * f[-1] )
+
+F = p * trapets 
+f_points = p * (H - y) * w
+print(f'Den resulterande kraften är = {trapets} N')
+
+
+plt.figure(figsize=(8,5))
+plt.plot(y, f, marker = 'o', label = "f(y) = (H-y) * w(y)")
+plt.xlabel("y [m]")
+plt.ylabel("f(y) [N/m]")
+plt.title("Integranden för dammkraften")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+T_h = trapets(f, 0, 12, 6)   
+T_2h = trapets(f, 0, 12, 3)
+E_h = (T_h - T_2h) / ((2**2)-1)
+print(f'Diskritiseringsfelet = {E_h} N')
